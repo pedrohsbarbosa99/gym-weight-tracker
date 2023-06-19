0735fff68f42458505426cd6054cd60a6c17b5c3
Pedro Barbosa <pedro.barbosa@nappsolutions.com>
2023-06-19 11:54:33 -0300

    feat: add redirect to api/docs

--- gym_weight_tracker/urls.py
+++ gym_weight_tracker/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from gym_weight_tracker.api.main import api
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/api/docs", permanent=True)),
     path("admin/", admin.site.urls),
     path("api/", api.urls),
 ]
