0e5fe061a585227173a4c308a34769a4ad6e33f7
Pedro Barbosa <pedro.barbosa@nappsolutions.com>
2023-08-09 16:44:59 -0300

    feat: add db_index

--- gym_weight_tracker/core/models.py
+++ gym_weight_tracker/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Exercise(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, db_index=True)
 
     def __str__(self):
         return self.name
