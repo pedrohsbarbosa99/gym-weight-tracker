20c7885da09f4823147ede7a07edebb3c3e44f8b
Pedro Barbosa <pedro.barbosa@nappsolutions.com>
2023-08-10 10:42:57 -0300

    feat: update version api

--- gym_weight_tracker/api/main.py
+++ gym_weight_tracker/api/main.py
@@ -6,7 +6,7 @@ from gym_weight_tracker.api.progression.api import progression_router
 from gym_weight_tracker.api.user.api import user_router
 from gym_weight_tracker.api.food.api import food_router
 
-api = NinjaExtraAPI(title="Light Weight Baby", auth=JWTAuth())
+api = NinjaExtraAPI(title="Light Weight Baby", auth=JWTAuth(), version="0.0.2")
 api.add_router("/users", user_router, tags=["users"])
 api.add_router("/exercises", exercise_router, tags=["exercises"])
 api.add_router("", progression_router, tags=["progressions"])
