409603da787aa8b6324cd84677f7f11467167029
Pedro Barbosa <pedro.barbosa@nappsolutions.com>
2023-08-17 09:19:19 -0300

    fix: remove redirect view from urls

--- gym_weight_tracker/urls.py
+++ gym_weight_tracker/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 from gym_weight_tracker.api.main import api
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="/api/docs", permanent=True)),
     path("admin/", admin.site.urls),
     path("api/", api.urls),
     path("__debug__/", include("debug_toolbar.urls")),
