3b3a543d3436398247b417bb10b7bdc77bffb761
Pedro Barbosa <pedrohsbarbosa99@gmail.com>
2023-08-26 12:24:01 -0300

    feat: improve similarity search exercises

--- gym_weight_tracker/core/apps.py
+++ gym_weight_tracker/core/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
+from django.db.models import CharField
+
+from .lookups import Similarity
 
 
 class CoreConfig(AppConfig):
+    CharField.register_lookup(Similarity)
     name = "gym_weight_tracker.core"
