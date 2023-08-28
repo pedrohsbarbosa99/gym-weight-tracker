2a28d00303035002990c6ecf7def9519fd9b7e9b
Pedro Barbosa <pedro.barbosa@nappsolutions.com>
2023-08-28 11:41:30 -0300

    feat: add similarity 0.5 search exercises

--- gym_weight_tracker/api/exercise/filters.py
+++ gym_weight_tracker/api/exercise/filters.py
@@ -1,8 +1,12 @@
 from typing import Optional
-
+from django.contrib.postgres.search import TrigramSimilarity
+from django.db.models import QuerySet
 from ninja import Field, FilterSchema
 
 
 class ExerciseFilterSchema(FilterSchema):
-    name__icontains: Optional[str]
     search: Optional[str] = Field(q=["name__unaccent__trigram_similar"])
+
+    def filter(self, queryset: QuerySet) -> QuerySet:
+        queryset = queryset.annotate(similarity=TrigramSimilarity("name", self.search))
+        return queryset.filter(self.get_filter_expression(), similarity__gt=0.5)
