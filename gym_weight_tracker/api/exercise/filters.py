d3a511c36fcad56a9aa07bc6b96fd2f9f0fcb5e7
Pedro Barbosa <pedro.barbosa@nappsolutions.com>
2023-08-28 16:58:46 -0300

    fix: remove Field from search

--- gym_weight_tracker/api/exercise/filters.py
+++ gym_weight_tracker/api/exercise/filters.py
@@ -1,11 +1,11 @@
 from typing import Optional
 from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models import QuerySet
-from ninja import Field, FilterSchema
+from ninja import FilterSchema
 
 
 class ExerciseFilterSchema(FilterSchema):
-    search: Optional[str] = Field(q=["name__unaccent__trigram_similar"])
+    search: Optional[str]
 
     def filter(self, queryset: QuerySet) -> QuerySet:
         if self.search:
