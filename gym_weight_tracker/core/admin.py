071b8cb0ed638c64c72d5d0d53068fbe294716ee
Pedro Barbosa <pedrohsbarbosa99@gmail.com>
2023-08-30 21:54:34 -0300

    feat: add first models to workout

--- gym_weight_tracker/core/admin.py
+++ gym_weight_tracker/core/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
 from gym_weight_tracker.account.models import User
-from gym_weight_tracker.core.models import Exercise, Progression
+from gym_weight_tracker.core.models import (
+    Exercise,
+    Progression,
+    Routine,
+    RoutineExercise,
+    RoutineExerciseExecution,
+)
 
 admin.site.register(Exercise)
+admin.site.register(Routine)
+admin.site.register(RoutineExercise)
+admin.site.register(RoutineExerciseExecution)
 admin.site.register(Progression)
 admin.site.register(User)
