5395d90ed44ee25ee3f448f366da623ccbf68d51
Pedro Barbosa <pedrohsbarbosa99@gmail.com>
2023-09-05 03:30:11 -0300

    refact: social authenticate backend

--- gym_weight_tracker/api/social/api.py
+++ gym_weight_tracker/api/social/api.py
@@ -1,9 +1,12 @@
 from django.core.handlers.wsgi import WSGIRequest
 from ninja.router import Router
-from ninja_jwt.tokens import RefreshToken
 
-from .schema import ConvertTokenSchema, Plataform, TokenSchema
-from .utils import import_social_validator
+from .schema import (
+    ConvertTokenSchema,
+    Plataform,
+    TokenSchema,
+    TokenSocialInput,
+)
 
 social_router = Router()
 
@@ -16,15 +19,6 @@ social_router = Router()
 def convert_token(
     request: WSGIRequest,
     plataform: Plataform,
-    payload: ConvertTokenSchema,
+    user_token: TokenSocialInput,
 ):
-    validator = import_social_validator(plataform)
-    user = validator.validate(payload.token)
-
-    refresh = RefreshToken.for_user(user)
-
-    return {
-        "email": user.email,
-        "refresh": str(refresh),
-        "access": str(refresh.access_token),
-    }
+    return ConvertTokenSchema.output_schema(plataform, user_token)
