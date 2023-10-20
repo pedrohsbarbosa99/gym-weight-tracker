3abd35c906371b078772428e2f30955a22fbf4e6
Pedro Barbosa <pedro.barbosa@nappsolutions.com>
2023-10-19 22:22:58 -0300

    refact: change how auth_router works

--- gym_weight_tracker/api/main.py
+++ gym_weight_tracker/api/main.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from functools import partial
 
 from ninja import NinjaAPI
 from ninja_extra import exceptions
@@ -9,26 +9,14 @@ from gym_weight_tracker.api.nutrition.api import nutrition_router
 from gym_weight_tracker.api.progression.api import progression_router
 from gym_weight_tracker.api.social.api import social_router
 from gym_weight_tracker.api.user.api import user_router
+from gym_weight_tracker.exception_handlers import api_exception_handler
 
 api = NinjaAPI(title="Light Weight Baby", version="0.0.3")
 
 
-def api_exception_handler(request, exc):
-    headers: Dict = {}
-
-    if isinstance(exc.detail, (list, dict)):
-        data = exc.detail
-    else:
-        data = {"detail": exc.detail}
-
-    response = api.create_response(request, data, status=exc.status_code)
-    for k, v in headers.items():
-        response.setdefault(k, v)
-
-    return response
-
-
-api.add_exception_handler(exceptions.APIException, api_exception_handler)
+api.add_exception_handler(
+    exceptions.APIException, partial(api_exception_handler, api=api)
+)
 api.add_router("/users", user_router, tags=["users"])
 api.add_router("/exercises", exercise_router, tags=["exercises"])
 api.add_router("", progression_router, tags=["progressions"])
